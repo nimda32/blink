88a940779f558531eaa777979725e3d79151c371
HolyPkerz <holypkerz@gmail.com>
2016-10-31 01:41:41 -0700

    Core Movement Added
    1. Added Left/Right movement (a + d)
    2. Added Jumping (w)
    3. Added teleport ability with wall collisions (left shift)
    4. Added camera follow
    5. Created a small testing platform level

---
+++ Blink/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	
+	public GameObject cameraTarget; // object to look at or follow
+	public GameObject player; // player object for moving
+	
+	public float smoothTime = 0.1f;    // time for dampen
+	public bool cameraFollowX = true; // camera follows on horizontal
+	public bool cameraFollowY = true; // camera follows on vertical
+	public bool cameraFollowHeight = true; // camera follow CameraTarget object height
+	public float cameraHeight = 50.0f; // height of camera adjustable
+	Vector2 velocity = new Vector2(5,5); // speed of camera movement
+	
+	private Transform thisTransform; // camera Transform
+	
+	// Use this for initialization
+	void Start()
+	{
+		cameraTarget = GameObject.Find ("Player");
+		thisTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		if (cameraFollowX)
+		{
+			thisTransform.position = new Vector3(Mathf.SmoothDamp(thisTransform.position.x, cameraTarget.transform.position.x, ref velocity.x, smoothTime), thisTransform.position.y, thisTransform.position.z);
+		}
+		if(cameraFollowY){
+			thisTransform.position = new Vector3(thisTransform.position.x, Mathf.SmoothDamp(thisTransform.position.y, cameraTarget.transform.position.y, ref velocity.y, smoothTime), thisTransform.position.z);
+		}
+	}
+}
---
+++ Blink/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public int speed = 10;
+	private int jumpHeight = 620;
+	int direction = 0; //0-right, 1-left
+	Vector3 dir = new Vector3(1, 0, 0);
+	Vector3 teleDist = new Vector3 (5, 0, 0);
+	Rigidbody r;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		r = GetComponent<Rigidbody>();
+	}
+
+	bool isGrounded()
+	{
+		return (r.velocity.y >= -0.01f && r.velocity.y <= 0.01f);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Left-Right Movement
+		if(Input.GetKey(KeyCode.A)) {
+			dir += transform.right * Time.deltaTime * -speed;
+			direction = 1;
+		}
+		else if(Input.GetKey(KeyCode.D)) {
+			dir += transform.right * Time.deltaTime * speed;
+			direction = 0;
+		}
+		r.MovePosition (transform.position + dir);
+		dir = new Vector3 (0, 0, 0);
+
+		//Jumping
+		if (Input.GetKeyDown (KeyCode.W)) 
+		{
+			if (isGrounded ())
+			{
+				r.AddForce(new Vector3(0, jumpHeight, 0));
+			}
+		}
+
+		//Teleporting
+		if (Input.GetKeyDown(KeyCode.LeftShift))
+		{
+			RaycastHit hit;
+			//teleport right
+			if (direction == 0)
+			{
+				//Check if there's anything in front of you that you might get stuck in
+				if (Physics.Raycast(transform.position, Vector3.right, out hit, 5.0f))
+				{
+					//if there is, stop in front of it
+					r.MovePosition (transform.position + new Vector3(hit.distance, 0, 0));
+				}
+				else
+				{
+					//otherwise teleport normally
+					r.MovePosition (transform.position + teleDist);
+				}
+			}
+			//teleport left
+			else if (direction == 1)
+			{
+				//Check if there's anything in front of you that you might get stuck in
+				if (Physics.Raycast(transform.position, Vector3.left, out hit, 5.0f))
+				{
+					//if there is, stop in front of it
+					r.MovePosition (transform.position - new Vector3(hit.distance, 0, 0));
+				}
+				else
+				{
+					//otherwise teleport normally
+					r.MovePosition (transform.position - teleDist);
+				}
+			}
+		}
+				
+	}
+}
