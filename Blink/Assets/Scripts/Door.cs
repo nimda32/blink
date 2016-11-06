839152f217cba066a7a24d91e2f2cb90cbeeccc6
HolyPkerz <holypkerz@gmail.com>
2016-11-06 01:32:59 -0800

    Switch and Door logic added, animation changes not addded in code yet

---
+++ Blink/Assets/Scripts/Door.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Door : MonoBehaviour {
+
+	bool locked = true; //self explanatory
+	public List<GameObject> switches; //list of switches needed to be pressed for door to unlock
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//Check if all the switches have been pressed
+	public void checkSwitches()
+	{
+		Switch s;
+		bool unlock = true;
+		for (int i = 0; i < switches.Count; i++) 
+		{
+			//get the Switch instance of the switch object
+			s = switches [i].GetComponent<Switch>(); 
+			//check if that switch has been pressed
+			if (s.isPressed() == false) 
+			{
+				unlock = false;
+			}
+		}
+		locked = !unlock;
+	}
+
+	public bool isUnlocked()
+	{
+		return !locked;
+	}
+}
