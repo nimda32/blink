400666771cb722ae5fbdc7f8a12b73a01926c55e
nimda <nimda@nimda.local>
2016-11-08 01:12:21 -0800

    added timed jump traps

---
+++ Blink/Assets/Scripts/TimedJumpTrap.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedJumpTrap : MonoBehaviour {
+
+	public GameObject wall;
+
+	void Start () {
+		StartCoroutine (toggleDeathWall());
+		
+	}
+		
+	bool isWallActive = false;
+
+	IEnumerator toggleDeathWall() {
+		for(;;) {
+
+			wall.SetActive( isWallActive );
+			isWallActive = !isWallActive;
+//			Debug.Log ("death is" + isWallActive);
+			yield return new WaitForSeconds(1f);
+		}
+	}
+}
