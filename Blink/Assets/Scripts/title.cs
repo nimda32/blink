2ea1caf5a4e52dfe0a324d86ee58ed77908a4b00
nimda <nimda@nimda.local>
2016-11-08 21:03:19 -0800

    added intro screen

---
+++ Blink/Assets/Scripts/title.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class title : MonoBehaviour {
+
+	public Button start; // assign in the editor
+
+
+	void Start() {
+		start.onClick.AddListener(() => { start_onClick(); });
+	}
+
+	void start_onClick(){
+		SceneManager.LoadScene("Level1",LoadSceneMode.Single);
+	}
+
+}
